27f3566556428b091954fcfd07290350b8310da5
tito <tito2112230@gmail.com>
2022-06-11 22:20:17 -0500

    "actualizado 11-06-2022"

--- ejercicio3MasFunci.js
+++ ejercicio3MasFunci.js
@@ -22,3 +22,55 @@ let calcularSalario = (horasTrabajadas) => {
     return salarioHorasComunes + salarioHorasExtras
 }
 console.log(calcularSalario(7))
+
+// if else
+let probarIf = (numero) => {
+    if (numero > 5){
+        console.log("numero es mayor que 5");
+    }else if (numero === 5){
+        console.log("numero es igual a 5");
+    }else {
+        console.log("entro en el ELSE");
+    }
+}
+//probarIf(20)
+
+let numero = prompt("ingrese un numero para comprobar: ")
+probarIf(parseInt(numero))
+
+// if ternario blanco o negro
+let clima = "lluvioso"
+
+let mamaSalgoConParaguas = clima ==="lluvioso" ? "si llevalo" : "no hace falta"
+console.log(mamaSalgoConParaguas);
+
+//swithc /cuando tenemos varias opciones
+let hacerSonidoAnimal = (animal)=> {
+    switch(animal){
+        case "gato":
+            return "miauu";
+        case "perro":
+            return "guuauuug";
+        case "gallo":
+            return "kikiriki";
+        default:
+            return "dilculpa no te distingo"
+    }
+}
+console.log(hacerSonidoAnimal("gallo"));
+
+
+//en un parque de diversiones nos piden un programa para verificar si los pasajeros de
+//la montaña rusa pueden subir (debe medir mas o igual a 1.40 y menos de 2 metros.
+//si mide menos de 1.40 hasta 1.20 devera venir acompañado
+//si mide menos de 1.20, no podra subir ni acompañado)
+let puedeSubir = ( altura, vieneAcompañada)=>{
+    if(altura >= 1.4 && altura < 2){
+        return "si puede subir....";
+    }else if(altura < 1.40 && altura >= 1.20 && vieneAcompañada === true){
+        return "si puede subir"
+    }else{
+        return "perdon no puede subir"
+    }
+}
+console.log(puedeSubir(1.90, "false"))
